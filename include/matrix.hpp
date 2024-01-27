61711c20f507d8432d6dfb393d896f5883b6bb88
Grant <christiansengrant18@gmail.com>
2024-01-27 10:38:49 -0600

    general update to matrix code

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -13,9 +13,16 @@ private:
     size_t cols;
     std::vector<T> vals;
 
+    bool inplaceInvert2();
+    bool inplaceInvert3();
+    bool inplaceInvert4();
+    bool inplaceInvertN();
+
 public:
     Matrix(size_t rows, size_t cols);
     Matrix(std::vector<T> vals, size_t cols);
+
+    static Matrix<T> identity(size_t n);
 
     inline T getIndex(size_t i, size_t j) const;
     inline T& getIndexRef(size_t i, size_t j);
@@ -25,10 +32,12 @@ public:
     void inplaceRowScale(size_t row, T scalar);
     void inplaceRowAdd(size_t add_row, size_t to_row);
     void inplaceScaledRowAdd(size_t add_row, T scaled_by, size_t to_row);
+
+    bool inplaceInvert();
 };
 
 /// @brief Creates a new zero matrix with the given number of rows and columns
-/// @tparam T The type of the data contained in the Matrix
+/// @tparam T The type of the contained `Matrix<T>` data
 /// @param rows The number of rows the matrix should have
 /// @param cols The number of columns the matrix should have
 template<typename T>
@@ -41,7 +50,7 @@ Matrix<T>::Matrix(size_t rows, size_t cols): rows(rows), cols(cols)
 
 /// @brief Creates a new `Matrix<T>` from the data in `vals`, but only 
 /// if the number of elements in `vals` is evenly divisible by `cols`
-/// @tparam T The type of the data contained in the Matrix
+/// @tparam T The type of the contained `Matrix<T>` data
 /// @param vals The data that the matrix should contain
 /// @param cols The number of columns the data should be divided into
 template<typename T>
@@ -54,18 +63,46 @@ Matrix<T>::Matrix(std::vector<T> vals, size_t cols): cols(cols), vals(vals)
     rows = vals.size() / cols;
 }
 
+/// @brief Creates a new identity `Matrix<T>` with `n` rows and `n` columns.
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param n The number of rows and columns that the matrix should have
+/// @return A new identity matrix
+template<typename T>
+Matrix<T> Matrix<T>::identity(size_t n)
+{
+    Matrix<T> res = Matrix(n, n);
+    for (size_t i = 0; i < n; i++)
+    {
+        res.getIndexRef(i, i) = (T)1;
+    }
+    return res;
+}
+
+/// @brief Returns the value stored at the `i`th row and `j`th column of the `Matrix<T>`
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param i The row to be accessed
+/// @param j The column to be accessed
+/// @returns The value stored at the given index
 template<typename T>
 inline T Matrix<T>::getIndex(size_t i, size_t j) const
 {
     return vals[i * cols + j];
 }
 
+/// @brief Returns a reference to the value stored at the `i`th row and `j`th column of the `Matrix<T>`
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param i The row to be accessed
+/// @param j The column to be accessed
+/// @returns The a reference to the value stored at the given index
 template<typename T>
 inline T& Matrix<T>::getIndexRef(size_t i, size_t j)
 {
     return vals[i * cols + j];
 }
 
+/// @brief Applies a function to each element in the `Matrix<T>`, mutating the element's value to the returned result
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param func The function that should be applied to each element of the `Matrix<T>`
 template<typename T>
 void Matrix<T>::inplaceMapOver(std::function<T (T)> func)
 {
@@ -75,6 +112,10 @@ void Matrix<T>::inplaceMapOver(std::function<T (T)> func)
     }
 }
 
+/// @brief Swaps the rows `r1` and `r2` in the `Matrix<T>`
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param r1 The first row to swap
+/// @param r2 The second row to swap
 template<typename T>
 void Matrix<T>::inplaceRowSwap(size_t r1, size_t r2)
 {
@@ -86,6 +127,10 @@ void Matrix<T>::inplaceRowSwap(size_t r1, size_t r2)
     }
 }
 
+/// @brief Scales the given row in the `Matrix<T>` by the given scalar value
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param row The row to scale
+/// @param scalar The quantity to scale the row by
 template<typename T>
 void Matrix<T>::inplaceRowScale(size_t row, T scalar)
 {
@@ -95,6 +140,10 @@ void Matrix<T>::inplaceRowScale(size_t row, T scalar)
     }
 }
 
+/// @brief Adds one row to another row in an element-wise fashion, mutating the values in the second row
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param add_row The row whose values should be added and not mutated
+/// @param to_row The row whose values should be mutably added to
 template<typename T>
 void Matrix<T>::inplaceRowAdd(size_t add_row, size_t to_row)
 {
@@ -104,6 +153,11 @@ void Matrix<T>::inplaceRowAdd(size_t add_row, size_t to_row)
     }
 }
 
+/// @brief Adds one row, scaled by a given value, to another row in an element-wise fashion, mutating the values in the second row
+/// @tparam T The type of the contained `Matrix<T>` data
+/// @param add_row The row whose values should be added and not mutated
+/// @param scaled_by The value to scale `add_row` by
+/// @param to_row The row whose values should be mutably added to
 template<typename T>
 void Matrix<T>::inplaceScaledRowAdd(size_t add_row, T scaled_by, size_t to_row)
 {
@@ -113,4 +167,9 @@ void Matrix<T>::inplaceScaledRowAdd(size_t add_row, T scaled_by, size_t to_row)
     }
 }
 
+template<typename T>
+bool Matrix<T>::inplaceInvert()
+{
+
+}
 #endif
