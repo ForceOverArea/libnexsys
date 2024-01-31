2abf6c87b524c7bff47d73cee39212d58a4e93a2
Grant <christiansengrant18@gmail.com>
2024-01-30 19:15:33 -0600

    changed naming to snake_case

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,5 +1,5 @@
-#ifndef MATRIX_HPP_
-#define MATRIX_HPP_
+#ifndef _MATRIX_HPP
+#define _MATRIX_HPP
 // NOTE: This header has no .cpp file counterpart to allow for ease of use with generics
 
 #include <vector>
@@ -13,10 +13,10 @@ private:
     Matrix();
 
     // Helper methods
-    bool inplaceInvert2() noexcept;
-    bool inplaceInvert3() noexcept;
-    bool inplaceInvert4() noexcept;
-    bool inplaceInvertN() noexcept;
+    bool inplace_invert_2() noexcept;
+    bool inplace_invert_3() noexcept;
+    bool inplace_invert_4() noexcept;
+    bool inplace_invert_n() noexcept;
 
 protected:
     size_t rows;
@@ -30,24 +30,26 @@ public:
 
     // Pseudo-constructors
     static Matrix<T> identity(size_t n);
-    Matrix<T> mapOver(std::function<T (T)> func);
+    Matrix<T> map_over(std::function<T (T)> func);
+    static Matrix<T> from_col_vec(std::vector<T>);
+    static Matrix<T> from_row_vec(std::vector<T>);
 
     // Getters and setters
-    inline T getIndex(size_t i, size_t j) const;
-    inline T& getIndexRef(size_t i, size_t j);
+    inline T get_index(size_t i, size_t j) const;
+    inline T& get_index_ref(size_t i, size_t j);
 
     // Operator overloads
     Matrix<T> operator*(T scalar) const;
     Matrix<T> operator*(Matrix<T> rhs) const;
 
     // Methods
-    void inplaceMapOver(std::function<T (T)> func);
-    void inplaceRowSwap(size_t r1, size_t r2);
-    void inplaceRowScale(size_t row, T scalar);
-    void inplaceRowAdd(size_t add_row, size_t to_row);
-    void inplaceScaledRowAdd(size_t add_row, T scaled_by, size_t to_row);
+    void inplace_map_over(std::function<T (T)> func);
+    void inplace_row_swap(size_t r1, size_t r2);
+    void inplace_row_scale(size_t row, T scalar);
+    void inplace_row_add(size_t add_row, size_t to_row);
+    void inplace_scaled_row_add(size_t add_row, T scaled_by, size_t to_row);
 
-    bool tryInplaceInvert() noexcept;
+    bool try_inplace_invert() noexcept;
 };
 
 /// Default constructor for a `Matrix<T>`, intended for internal class usage only
@@ -68,7 +70,7 @@ Matrix<T>::Matrix(size_t rows, size_t cols): rows(rows), cols(cols)
 {
     vals = std::vector<T>();
     vals.reserve(rows * cols);
-    this->inplaceMapOver([](T _elem){ return (T)0; });
+    this->inplace_map_over([](T _elem){ return (T)0; });
 }
 
 /// @brief Creates a new `Matrix<T>` from the data in `vals`, but only 
@@ -96,7 +98,7 @@ Matrix<T> Matrix<T>::identity(size_t n)
     Matrix<T> res = Matrix(n, n);
     for (size_t i = 0; i < n; i++)
     {
-        res.getIndexRef(i, i) = (T)1;
+        res.get_index_ref(i, i) = (T)1;
     }
     return res;
 }
@@ -106,9 +108,9 @@ Matrix<T> Matrix<T>::identity(size_t n)
 /// @param func The function to use to create the new matrix's elements from the old elements
 /// @return a new `Matrix<T>` containing the elements created by `func`
 template<typename T>
-Matrix<T> Matrix<T>::mapOver(std::function<T (T)> func)
+Matrix<T> Matrix<T>::map_over(std::function<T (T)> func)
 {
-    Matrix<T> res = Matrix();
+    Matrix<T> res;
     res.rows = rows;
     res.cols = cols;
 
@@ -120,13 +122,33 @@ Matrix<T> Matrix<T>::mapOver(std::function<T (T)> func)
     return res;
 }
 
+/// @brief Creates a new single-column `Matrix<T>` from a given `std::vector<T>`
+/// @tparam T The type of the contained `Matrix<T>` data 
+/// @param col The column vector values to construct the matrix with
+/// @return a new 1-column `Matrix<T>` instance
+template<typename T>
+Matrix<T> Matrix<T>::from_col_vec(std::vector<T> col)
+{
+    return Matrix<T>(col, 1);
+}
+
+/// @brief Creates a new single-row `Matrix<T>` from a given `std::vector<T>`
+/// @tparam T The type of the contained `Matrix<T>` data 
+/// @param row The row vector values to construct the matrix with
+/// @return a new 1-row `Matrix<T>` instance
+template<typename T>
+Matrix<T> Matrix<T>::from_row_vec(std::vector<T> row)
+{
+    return Matrix<T>(row, row.size());
+}
+
 /// @brief Returns the value stored at the `i`th row and `j`th column of the `Matrix<T>`
 /// @tparam T The type of the contained `Matrix<T>` data
 /// @param i The row to be accessed
 /// @param j The column to be accessed
 /// @returns The value stored at the given index
 template<typename T>
-inline T Matrix<T>::getIndex(size_t i, size_t j) const
+inline T Matrix<T>::get_index(size_t i, size_t j) const
 {
     return vals[i * cols + j];
 }
@@ -137,7 +159,7 @@ inline T Matrix<T>::getIndex(size_t i, size_t j) const
 /// @param j The column to be accessed
 /// @returns The a reference to the value stored at the given index
 template<typename T>
-inline T& Matrix<T>::getIndexRef(size_t i, size_t j)
+inline T& Matrix<T>::get_index_ref(size_t i, size_t j)
 {
     return vals[i * cols + j];
 }
@@ -149,7 +171,7 @@ inline T& Matrix<T>::getIndexRef(size_t i, size_t j)
 template<typename T>
 Matrix<T> Matrix<T>::operator*(T scalar) const
 {
-    return this->mapOver([scalar](T x){ return x * scalar; });
+    return this->map_over([scalar](T x){ return x * scalar; });
 }
 
 /// @brief Returns a new `Matrix<T>` containing the matrix product of two matrices
@@ -173,7 +195,7 @@ Matrix<T> Matrix<T>::operator*(Matrix<T> rhs) const
         {
             for (size_t x = 0; x < n; x++)
             {
-                res->getIndexRef(i, j) += this->getIndex(i, x) * rhs.getIndex(x, j);
+                res->get_index_ref(i, j) += this->get_index(i, x) * rhs.get_index(x, j);
             }
         }
     }
@@ -185,7 +207,7 @@ Matrix<T> Matrix<T>::operator*(Matrix<T> rhs) const
 /// @tparam T The type of the contained `Matrix<T>` data
 /// @param func The function that should be applied to each element of the `Matrix<T>`
 template<typename T>
-void Matrix<T>::inplaceMapOver(std::function<T (T)> func)
+void Matrix<T>::inplace_map_over(std::function<T (T)> func)
 {
     for (auto p = vals.begin(); p != vals.end(); p++)
     {
@@ -198,13 +220,13 @@ void Matrix<T>::inplaceMapOver(std::function<T (T)> func)
 /// @param r1 The first row to swap
 /// @param r2 The second row to swap
 template<typename T>
-void Matrix<T>::inplaceRowSwap(size_t r1, size_t r2)
+void Matrix<T>::inplace_row_swap(size_t r1, size_t r2)
 {
     for (size_t j = 0; j < cols; j++)
     {
-        T stor = this->getIndex(r1, j);
-        this->getIndexRef(r1, j) = this->getIndex(r2, j);
-        this->getIndexRef(r2, j) = stor;
+        T stor = this->get_index(r1, j);
+        this->get_index_ref(r1, j) = this->get_index(r2, j);
+        this->get_index_ref(r2, j) = stor;
     }
 }
 
@@ -213,11 +235,11 @@ void Matrix<T>::inplaceRowSwap(size_t r1, size_t r2)
 /// @param row The row to scale
 /// @param scalar The quantity to scale the row by
 template<typename T>
-void Matrix<T>::inplaceRowScale(size_t row, T scalar)
+void Matrix<T>::inplace_row_scale(size_t row, T scalar)
 {
     for (size_t j = 0; j < cols; j++)
     {
-        this->getIndexRef(row, j) *= scalar;
+        this->get_index_ref(row, j) *= scalar;
     }
 }
 
@@ -226,11 +248,11 @@ void Matrix<T>::inplaceRowScale(size_t row, T scalar)
 /// @param add_row The row whose values should be added and not mutated
 /// @param to_row The row whose values should be mutably added to
 template<typename T>
-void Matrix<T>::inplaceRowAdd(size_t add_row, size_t to_row)
+void Matrix<T>::inplace_row_add(size_t add_row, size_t to_row)
 {
     for (size_t j = 0; j < cols; j++)
     {
-        this->getIndexRef(to_row, j) += this->getIndex(add_row, j);
+        this->get_index_ref(to_row, j) += this->get_index(add_row, j);
     }
 }
 
@@ -240,22 +262,22 @@ void Matrix<T>::inplaceRowAdd(size_t add_row, size_t to_row)
 /// @param scaled_by The value to scale `add_row` by
 /// @param to_row The row whose values should be mutably added to
 template<typename T>
-void Matrix<T>::inplaceScaledRowAdd(size_t add_row, T scaled_by, size_t to_row)
+void Matrix<T>::inplace_scaled_row_add(size_t add_row, T scaled_by, size_t to_row)
 {
     for (size_t j = 0; j < cols; j++)
     {
-        this->getIndexRef(to_row, j) += this->getIndex(add_row, j) * scaled_by;
+        this->get_index_ref(to_row, j) += this->get_index(add_row, j) * scaled_by;
     }
 }
 
-/// @brief Helper function for `tryInplaceInvert`
+/// @brief Helper function for `try_inplace_invert`
 template<typename T>
-bool Matrix<T>::inplaceInvert2() noexcept
+bool Matrix<T>::inplace_invert_2() noexcept
 {
-    T a11 = this->getIndex(0, 0);
-    T a12 = this->getIndex(0, 1);
-    T a21 = this->getIndex(1, 0);
-    T a22 = this->getIndex(1, 1);
+    T a11 = this->get_index(0, 0);
+    T a12 = this->get_index(0, 1);
+    T a21 = this->get_index(1, 0);
+    T a22 = this->get_index(1, 1);
 
     T det = a11 * a22 - a12 * a21;
 
@@ -264,27 +286,27 @@ bool Matrix<T>::inplaceInvert2() noexcept
         return false;
     }
 
-    this->getIndexRef(0, 0) =   a22 / det;
-    this->getIndexRef(1, 0) = - a21 / det;
-    this->getIndexRef(0, 1) = - a12 / det;
-    this->getIndexRef(1, 1) =   a11 / det;
+    this->get_index_ref(0, 0) =   a22 / det;
+    this->get_index_ref(1, 0) = - a21 / det;
+    this->get_index_ref(0, 1) = - a12 / det;
+    this->get_index_ref(1, 1) =   a11 / det;
 
     return true;
 }
 
-/// @brief Helper function for `tryInplaceInvert`
+/// @brief Helper function for `try_inplace_invert`
 template<typename T>
-bool Matrix<T>::inplaceInvert3() noexcept
+bool Matrix<T>::inplace_invert_3() noexcept
 {
-    T a11 = this->getIndex(0, 0);
-    T a12 = this->getIndex(1, 0);
-    T a13 = this->getIndex(2, 0);
-    T a21 = this->getIndex(0, 1);
-    T a22 = this->getIndex(1, 1);
-    T a23 = this->getIndex(2, 1);
-    T a31 = this->getIndex(0, 2);
-    T a32 = this->getIndex(1, 2);
-    T a33 = this->getIndex(2, 2);
+    T a11 = this->get_index(0, 0);
+    T a12 = this->get_index(1, 0);
+    T a13 = this->get_index(2, 0);
+    T a21 = this->get_index(0, 1);
+    T a22 = this->get_index(1, 1);
+    T a23 = this->get_index(2, 1);
+    T a31 = this->get_index(0, 2);
+    T a32 = this->get_index(1, 2);
+    T a33 = this->get_index(2, 2);
 
     T det = a11*a22*a33 + a21*a32*a13 + a31*a12*a23 
           - a11*a32*a23 - a31*a22*a13 - a21*a12*a33;
@@ -294,39 +316,39 @@ bool Matrix<T>::inplaceInvert3() noexcept
         return false;
     }
 
-    this->getIndexRef(0, 0) = (a22 * a33 - a23 * a32) / det;
-    this->getIndexRef(1, 0) = (a23 * a31 - a21 * a33) / det;
-    this->getIndexRef(2, 0) = (a21 * a32 - a22 * a31) / det;
-    this->getIndexRef(0, 1) = (a13 * a32 - a12 * a33) / det;
-    this->getIndexRef(1, 1) = (a11 * a33 - a13 * a31) / det;
-    this->getIndexRef(2, 1) = (a12 * a31 - a11 * a32) / det;
-    this->getIndexRef(0, 2) = (a12 * a23 - a13 * a22) / det;
-    this->getIndexRef(1, 2) = (a13 * a21 - a11 * a23) / det;
-    this->getIndexRef(2, 2) = (a11 * a22 - a12 * a21) / det;
+    this->get_index_ref(0, 0) = (a22 * a33 - a23 * a32) / det;
+    this->get_index_ref(1, 0) = (a23 * a31 - a21 * a33) / det;
+    this->get_index_ref(2, 0) = (a21 * a32 - a22 * a31) / det;
+    this->get_index_ref(0, 1) = (a13 * a32 - a12 * a33) / det;
+    this->get_index_ref(1, 1) = (a11 * a33 - a13 * a31) / det;
+    this->get_index_ref(2, 1) = (a12 * a31 - a11 * a32) / det;
+    this->get_index_ref(0, 2) = (a12 * a23 - a13 * a22) / det;
+    this->get_index_ref(1, 2) = (a13 * a21 - a11 * a23) / det;
+    this->get_index_ref(2, 2) = (a11 * a22 - a12 * a21) / det;
 
     return true;
 }
 
-/// @brief Helper function for `tryInplaceInvert`
+/// @brief Helper function for `try_inplace_invert`
 template<typename T>
-bool Matrix<T>::inplaceInvert4() noexcept
+bool Matrix<T>::inplace_invert_4() noexcept
 {
-    T a11 = this->getIndex(0, 0);
-    T a12 = this->getIndex(1, 0);
-    T a13 = this->getIndex(2, 0);
-    T a14 = this->getIndex(3, 0);
-    T a21 = this->getIndex(0, 1);
-    T a22 = this->getIndex(1, 1);
-    T a23 = this->getIndex(2, 1);
-    T a24 = this->getIndex(3, 1);
-    T a31 = this->getIndex(0, 2);
-    T a32 = this->getIndex(1, 2);
-    T a33 = this->getIndex(2, 2);
-    T a34 = this->getIndex(3, 2);
-    T a41 = this->getIndex(0, 3);
-    T a42 = this->getIndex(1, 3);
-    T a43 = this->getIndex(2, 3);
-    T a44 = this->getIndex(3, 3);
+    T a11 = this->get_index(0, 0);
+    T a12 = this->get_index(1, 0);
+    T a13 = this->get_index(2, 0);
+    T a14 = this->get_index(3, 0);
+    T a21 = this->get_index(0, 1);
+    T a22 = this->get_index(1, 1);
+    T a23 = this->get_index(2, 1);
+    T a24 = this->get_index(3, 1);
+    T a31 = this->get_index(0, 2);
+    T a32 = this->get_index(1, 2);
+    T a33 = this->get_index(2, 2);
+    T a34 = this->get_index(3, 2);
+    T a41 = this->get_index(0, 3);
+    T a42 = this->get_index(1, 3);
+    T a43 = this->get_index(2, 3);
+    T a44 = this->get_index(3, 3);
 
     T det = a11*a22*a33*a44 + a11*a23*a34*a42 + a11*a24*a32*a43 +
             a12*a21*a34*a43 + a12*a23*a31*a44 + a12*a24*a33*a41 + 
@@ -342,29 +364,29 @@ bool Matrix<T>::inplaceInvert4() noexcept
         return false;
     }
 
-    this->getIndex(0, 0) = (a22*a33*a44 + a23*a34*a42 + a24*a32*a43 - a22*a34*a43 - a23*a32*a44 - a24*a33*a42) / det;
-    this->getIndex(1, 0) = (a12*a34*a43 + a13*a32*a44 + a14*a33*a42 - a12*a33*a44 - a13*a34*a42 - a14*a32*a43) / det;
-    this->getIndex(2, 0) = (a12*a23*a44 + a13*a24*a42 + a14*a22*a43 - a12*a24*a43 - a13*a22*a44 - a14*a23*a42) / det;
-    this->getIndex(3, 0) = (a12*a24*a33 + a13*a22*a34 + a14*a23*a32 - a12*a23*a34 - a13*a24*a32 - a14*a22*a33) / det;
-    this->getIndex(0, 1) = (a21*a34*a43 + a23*a31*a44 + a24*a33*a41 - a21*a33*a44 - a23*a34*a41 - a24*a31*a43) / det;
-    this->getIndex(1, 1) = (a11*a33*a44 + a13*a34*a41 + a14*a31*a43 - a11*a34*a43 - a13*a31*a44 - a14*a33*a41) / det;
-    this->getIndex(2, 1) = (a11*a24*a43 + a13*a21*a44 + a14*a23*a41 - a11*a23*a44 - a13*a24*a41 - a14*a21*a43) / det;
-    this->getIndex(3, 1) = (a11*a23*a34 + a13*a24*a31 + a14*a21*a33 - a11*a24*a33 - a13*a21*a34 - a14*a23*a31) / det;
-    this->getIndex(0, 2) = (a21*a32*a44 + a22*a34*a41 + a24*a31*a42 - a21*a34*a42 - a22*a31*a44 - a24*a32*a41) / det;
-    this->getIndex(1, 2) = (a11*a34*a42 + a12*a31*a44 + a14*a32*a41 - a11*a32*a44 - a12*a34*a41 - a14*a31*a42) / det;
-    this->getIndex(2, 2) = (a11*a22*a44 + a12*a24*a41 + a14*a21*a42 - a11*a24*a42 - a12*a21*a44 - a14*a22*a41) / det;
-    this->getIndex(3, 2) = (a11*a24*a32 + a12*a21*a34 + a14*a22*a31 - a11*a22*a34 - a12*a24*a31 - a14*a21*a32) / det;
-    this->getIndex(0, 3) = (a21*a33*a42 + a22*a31*a43 + a23*a32*a41 - a21*a32*a43 - a22*a33*a41 - a23*a31*a42) / det;
-    this->getIndex(1, 3) = (a11*a32*a43 + a12*a33*a41 + a13*a31*a42 - a11*a33*a42 - a12*a31*a43 - a13*a32*a41) / det;
-    this->getIndex(2, 3) = (a11*a23*a42 + a12*a21*a43 + a13*a22*a41 - a11*a22*a43 - a12*a23*a41 - a13*a21*a42) / det;
-    this->getIndex(3, 3) = (a11*a22*a33 + a12*a23*a31 + a13*a21*a32 - a11*a23*a32 - a12*a21*a33 - a13*a22*a31) / det;
+    this->get_index(0, 0) = (a22*a33*a44 + a23*a34*a42 + a24*a32*a43 - a22*a34*a43 - a23*a32*a44 - a24*a33*a42) / det;
+    this->get_index(1, 0) = (a12*a34*a43 + a13*a32*a44 + a14*a33*a42 - a12*a33*a44 - a13*a34*a42 - a14*a32*a43) / det;
+    this->get_index(2, 0) = (a12*a23*a44 + a13*a24*a42 + a14*a22*a43 - a12*a24*a43 - a13*a22*a44 - a14*a23*a42) / det;
+    this->get_index(3, 0) = (a12*a24*a33 + a13*a22*a34 + a14*a23*a32 - a12*a23*a34 - a13*a24*a32 - a14*a22*a33) / det;
+    this->get_index(0, 1) = (a21*a34*a43 + a23*a31*a44 + a24*a33*a41 - a21*a33*a44 - a23*a34*a41 - a24*a31*a43) / det;
+    this->get_index(1, 1) = (a11*a33*a44 + a13*a34*a41 + a14*a31*a43 - a11*a34*a43 - a13*a31*a44 - a14*a33*a41) / det;
+    this->get_index(2, 1) = (a11*a24*a43 + a13*a21*a44 + a14*a23*a41 - a11*a23*a44 - a13*a24*a41 - a14*a21*a43) / det;
+    this->get_index(3, 1) = (a11*a23*a34 + a13*a24*a31 + a14*a21*a33 - a11*a24*a33 - a13*a21*a34 - a14*a23*a31) / det;
+    this->get_index(0, 2) = (a21*a32*a44 + a22*a34*a41 + a24*a31*a42 - a21*a34*a42 - a22*a31*a44 - a24*a32*a41) / det;
+    this->get_index(1, 2) = (a11*a34*a42 + a12*a31*a44 + a14*a32*a41 - a11*a32*a44 - a12*a34*a41 - a14*a31*a42) / det;
+    this->get_index(2, 2) = (a11*a22*a44 + a12*a24*a41 + a14*a21*a42 - a11*a24*a42 - a12*a21*a44 - a14*a22*a41) / det;
+    this->get_index(3, 2) = (a11*a24*a32 + a12*a21*a34 + a14*a22*a31 - a11*a22*a34 - a12*a24*a31 - a14*a21*a32) / det;
+    this->get_index(0, 3) = (a21*a33*a42 + a22*a31*a43 + a23*a32*a41 - a21*a32*a43 - a22*a33*a41 - a23*a31*a42) / det;
+    this->get_index(1, 3) = (a11*a32*a43 + a12*a33*a41 + a13*a31*a42 - a11*a33*a42 - a12*a31*a43 - a13*a32*a41) / det;
+    this->get_index(2, 3) = (a11*a23*a42 + a12*a21*a43 + a13*a22*a41 - a11*a22*a43 - a12*a23*a41 - a13*a21*a42) / det;
+    this->get_index(3, 3) = (a11*a22*a33 + a12*a23*a31 + a13*a21*a32 - a11*a23*a32 - a12*a21*a33 - a13*a22*a31) / det;
 
     return true;
 }
 
-/// @brief Helper function for `tryInplaceInvert`
+/// @brief Helper function for `try_inplace_invert`
 template<typename T>
-bool Matrix<T>::inplaceInvertN() noexcept
+bool Matrix<T>::inplace_invert_n() noexcept
 {
     size_t n = rows;
     Matrix<T> inv = Matrix<T>::identity(n);
@@ -379,23 +401,23 @@ bool Matrix<T>::inplaceInvertN() noexcept
                 continue;
             }
 
-            if (this->getIndex(i, j) == (T)0)
+            if (this->get_index(i, j) == (T)0)
             {
                 vals = backup;
                 return false;
             }
 
-            T scalar = this->getIndex(i, j) / this->getIndex(j, j);
-            this->inplaceRowAdd(i, j, -scalar);
-            inv.inplaceRowAdd(i, j, -scalar);
+            T scalar = this->get_index(i, j) / this->get_index(j, j);
+            this->inplace_row_add(i, j, -scalar);
+            inv.inplace_row_add(i, j, -scalar);
         }
     }
 
     for (size_t i = 0; i < n; i++)
     {
-        T scalar = ((T)1) / this->getIndex(i, i);
-        this->inplaceRowScale(i, scalar);
-        inv.inplaceRowScale(i, scalar);
+        T scalar = ((T)1) / this->get_index(i, i);
+        this->inplace_row_scale(i, scalar);
+        inv.inplace_row_scale(i, scalar);
     }
 
     this->vals = inv.vals;
@@ -407,7 +429,7 @@ bool Matrix<T>::inplaceInvertN() noexcept
 /// be in the same state as it was prior to the inversion attempt.
 /// @returns A bool indicating if inversion was successful.
 template<typename T>
-bool Matrix<T>::tryInplaceInvert() noexcept
+bool Matrix<T>::try_inplace_invert() noexcept
 {
     if (rows != cols)
     {
@@ -422,16 +444,16 @@ bool Matrix<T>::tryInplaceInvert() noexcept
             return true;
             
         case 2:
-            return inplaceInvert2();
+            return inplace_invert_2();
 
         case 3:
-            return inplaceInvert3();
+            return inplace_invert_3();
 
         case 4:
-            return inplaceInvert4();
+            return inplace_invert_4();
 
         default:
-            return inplaceInvertN();
+            return inplace_invert_n();
     }
 }
 
